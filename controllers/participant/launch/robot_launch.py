8dcb6b4b7fe46535e9a13e14b881fde0014ee0a9
Léo Duggan <leo.duggan@alumni.epfl.ch>
2023-02-20 10:15:10 +0100

    Better management of empty WEBOTS_CONTROLLER_URL

--- controllers/participant/launch/robot_launch.py
+++ controllers/participant/launch/robot_launch.py
@@ -9,11 +9,14 @@ def generate_launch_description():
     package_dir = get_package_share_directory('participant')
     robot_description = pathlib.Path(os.path.join(package_dir, 'resource', 'webots_controller.urdf')).read_text()
 
+    controller_url = os.environ.get('WEBOTS_CONTROLLER_URL')
+    additional_env = {'WEBOTS_CONTROLLER_URL': controller_url} if controller_url else None
+
     my_robot_driver = Node(
         package='webots_ros2_driver',
         executable='driver',
         output='screen',
-        additional_env={'WEBOTS_CONTROLLER_URL': os.environ['WEBOTS_CONTROLLER_URL']},
+        additional_env=additional_env,
         parameters=[
             {'robot_description': robot_description},
         ]
