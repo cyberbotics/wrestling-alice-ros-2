f4e12a753f5965e2e2582ab51980b1611361762a
Léo Duggan <leo.duggan@alumni.epfl.ch>
2023-03-09 10:53:22 +0100

    Playing the hand wave motion

--- controllers/participant/participant/nao_controller.py
+++ controllers/participant/participant/nao_controller.py
@@ -1,18 +1,17 @@
 import rclpy
 
+# This is a workaround so that Webots' Python controller classes can be used
+# in this case, we need it to import the Motion class
+import os
+from ament_index_python.packages import get_package_prefix
+os.environ['WEBOTS_HOME'] = get_package_prefix('webots_ros2_driver')
+from controller import Motion
 
 class NaoDriver:
     def init(self, webots_node, properties):
-        self.__robot = webots_node.robot
-
-        # we initialize the shoulder pitch motors using the Robot.getDevice() function:
-        self.__RShoulderPitch = self.__robot.getDevice("RShoulderPitch")
-        self.__LShoulderPitch = self.__robot.getDevice("LShoulderPitch")
-        self.__RShoulderRoll = self.__robot.getDevice("RShoulderRoll")
-
-        # to control a motor, we use the setPosition() function:
-        self.__RShoulderPitch.setPosition(-1.3)
-        self.__LShoulderPitch.setPosition(1.3)
+        # we play on loop the HandWave motion
+        self.handWave = Motion('../motions/HandWave.motion')
+        self.handWave.setLoop(True)
 
         rclpy.init(args=None)
         self.__node = rclpy.create_node('nao_driver')
@@ -20,8 +19,4 @@ class NaoDriver:
     def step(self):
         # Mandatory function to go to the next simulation step
         rclpy.spin_once(self.__node, timeout_sec=0)
-
-        if self.__robot.getTime() % 2 < 1:
-            self.__RShoulderRoll.setPosition(0)
-        else:
-            self.__RShoulderRoll.setPosition(-0.3)
+        self.handWave.play()
