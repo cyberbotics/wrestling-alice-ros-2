7d6847dfec8b19c40008a71043cde19f986bb307
Olivier Michel <Olivier.Michel@cyberbotics.com>
2023-07-24 13:58:23 +0200

    Uses the WebotsController launcher

--- controllers/participant/launch/robot_launch.py
+++ controllers/participant/launch/robot_launch.py
@@ -1,24 +1,16 @@
 import os
-import pathlib
-from launch_ros.actions import Node
 from launch import LaunchDescription
 from ament_index_python.packages import get_package_share_directory
-
+from webots_ros2_driver import WebotsController
 
 def generate_launch_description():
     package_dir = get_package_share_directory('participant')
-    robot_description = pathlib.Path(os.path.join(package_dir, 'resource', 'webots_controller.urdf')).read_text()
-
     controller_url = os.environ.get('WEBOTS_CONTROLLER_URL')
-    additional_env = {'WEBOTS_CONTROLLER_URL': controller_url} if controller_url else None
 
-    my_robot_driver = Node(
-        package='webots_ros2_driver',
-        executable='driver',
-        output='screen',
-        additional_env=additional_env,
+    my_robot_driver = WebotsController(
+        robot_name=controller_url,
         parameters=[
-            {'robot_description': robot_description},
+            {'robot_description': os.path.join(package_dir, 'resource', 'webots_controller.urdf')},
         ]
     )
 
