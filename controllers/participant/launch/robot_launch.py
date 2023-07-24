524f9ab3eca7a8d1acec17a060ef70aa26f0d0f6
Olivier Michel <Olivier.Michel@cyberbotics.com>
2023-07-24 14:01:25 +0200

    Update robot_launch.py

--- controllers/participant/launch/robot_launch.py
+++ controllers/participant/launch/robot_launch.py
@@ -1,7 +1,7 @@
 import os
 from launch import LaunchDescription
 from ament_index_python.packages import get_package_share_directory
-from webots_ros2_driver import WebotsController
+from webots_ros2_driver.webots_controller import WebotsController
 
 def generate_launch_description():
     package_dir = get_package_share_directory('participant')
